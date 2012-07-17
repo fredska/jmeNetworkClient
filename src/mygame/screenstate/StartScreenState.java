862c9feed8d909d8679b357acb490c0c4850a6d4
Fred Skallos <fskallos@aeoniansoftware.com>
2012-07-17 16:45:31 -0400

    Daily Commit 7-14

---
+++ src/mygame/screenstate/StartScreenState.java
@@ -0,0 +1,60 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mygame.screenstate;
+
+import com.jme3.app.Application;
+import com.jme3.app.SimpleApplication;
+import com.jme3.app.state.AbstractAppState;
+import com.jme3.app.state.AppStateManager;
+import com.jme3.asset.AssetManager;
+import com.jme3.math.ColorRGBA;
+import com.jme3.renderer.ViewPort;
+import com.jme3.scene.Node;
+
+/**
+ *
+ * @author root
+ */
+public class StartScreenState extends AbstractAppState{
+    private ViewPort viewPort;
+    private Node rootNode;
+    private Node guiNode;
+    private AssetManager assetManager;
+    private Node localRootNode = new Node("Start Screen RootNode");
+    private Node localGuiNode = new Node("Start Screen GuiNode");
+    private final ColorRGBA backgroundColor = ColorRGBA.Gray;
+    
+    public StartScreenState(SimpleApplication app) {
+        this.rootNode = app.getRootNode();
+        this.viewPort = app.getViewPort();
+        this.guiNode = app.getGuiNode();
+        this.assetManager = app.getAssetManager();
+    }
+    
+    @Override
+    public void initialize(AppStateManager stateManager, Application app) {
+        super.initialize(stateManager, app);
+        /** init the screen */    
+    }
+
+    @Override
+    public void update(float tpf) {
+        /** any main loop action happens here */
+    }
+
+    @Override
+    public void stateAttached(AppStateManager stateManager) {
+        rootNode.attachChild(localRootNode);
+        guiNode.attachChild(localGuiNode);
+        viewPort.setBackgroundColor(backgroundColor);
+    }
+
+    @Override
+    public void stateDetached(AppStateManager stateManager) {
+        rootNode.detachChild(localRootNode);
+        guiNode.detachChild(localGuiNode);
+    }
+
+}
---
+++ src/mygame/screenstate/StartScreenStateController.java
@@ -0,0 +1,49 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mygame.screenstate;
+
+import com.jme3.app.Application;
+import com.jme3.app.SimpleApplication;
+import com.jme3.app.state.AbstractAppState;
+import com.jme3.app.state.AppStateManager;
+import de.lessvoid.nifty.Nifty;
+import de.lessvoid.nifty.screen.Screen;
+import de.lessvoid.nifty.screen.ScreenController;
+
+/**
+ *
+ * @author root
+ */
+public class StartScreenStateController extends AbstractAppState implements ScreenController{
+
+    private Nifty nifty;
+    private Screen screen;
+    private SimpleApplication app;
+    
+    public void bind(Nifty nifty, Screen screen) {
+        this.nifty = nifty;
+        this.screen = screen;
+    }
+
+    public void onStartScreen() {
+        //throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public void onEndScreen() {
+        //throw new UnsupportedOperationException("Not supported yet.");
+    }
+    
+    @Override
+    public void initialize(AppStateManager stateManager, Application app) {
+        super.initialize(stateManager, app);
+        this.app=(SimpleApplication)app;
+    }
+    
+    @Override
+    public void update(float tpf){
+        /** jME update loop **/
+    }
+    
+}
